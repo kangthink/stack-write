f0085a8f1f61a712d8775b96c68845bba9ace376
Alex <kangthink@insighter.co.kr>
2021-08-17 18:00:20 +0900

    Init project

---
+++ src/domain/sample.spec.js
@@ -0,0 +1,7 @@
+const { expect } = require("chai");
+
+describe("aa", () => {
+  it("aa", () => {
+    expect(4).to.be.equal(5);
+  });
+});
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import { helloWorld } from "./hello.js";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
